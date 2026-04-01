191d0886fa9d64bd1675ae6556e5385be31747b6
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-04-01 11:16:57 -0300

    Add files via upload

---
+++ assets/js/sidebar.js
@@ -0,0 +1,26 @@
+
+(function () {
+  function renderSidebar(container) {
+    if (!window.MENU_CONFIG || !window.MENU_CONFIG.adm) {
+      console.error("MENU_CONFIG não carregado");
+      return;
+    }
+
+    const menu = window.MENU_CONFIG.adm;
+
+    container.innerHTML = `
+      <div class="sidebar">
+        ${menu.map(setor => `
+          <div class="menu-setor">
+            <div class="menu-title">${setor.nome}</div>
+            <div class="submenu">
+              ${(setor.modulos || []).map(m => `<div class="submenu-item">${m}</div>`).join("")}
+            </div>
+          </div>
+        `).join("")}
+      </div>
+    `;
+  }
+
+  window.SIDEBAR = { renderSidebar };
+})();
