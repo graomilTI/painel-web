027e0557166491f1f8522fd882472d5729fd860a
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-04-29 11:59:34 -0300

    Add files via upload

--- assets/js/menuConfig.js
+++ assets/js/menuConfig.js
@@ -101,7 +101,7 @@ export const MENU_CONFIG = [
   {
     grupo: "DIRETORIA",
     itens: [
-      item("DRE","dre","Dre",["DRE"]),
+      item("diretoria_dre", "DRE", "dre", ["DRE", "DIRETORIA_DRE"]),
       item("financeiro", "Financeiro", "financeiro", ["FINANCEIRO"]),
       item("usuarios_acessos", "Usuários e Acessos", "admin-usuarios", ["ADMIN_USUARIOS", "USUARIOS_E_ACESSOS"]),
       item("admin_config", "Configurações", "admin-configuracoes", ["ADMIN_CONFIG"])
