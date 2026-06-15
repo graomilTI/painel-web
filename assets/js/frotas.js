8a0c7edf370769fb4851a8d43a9f9c461fe9e4cb
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-15 12:01:20 -0300

    feat: aplicar período padrão da semana anterior

--- assets/js/frotas.js
+++ assets/js/frotas.js
@@ -2,10 +2,12 @@ import { initProtectedPage } from './pageInit.js';
 import { supabase } from './supabaseClient.js';
 import { installDailyDriverResolution } from './frotas-motorista-leitura-diaria.js?v=20260615d';
 import { installPrintDriverValidation } from './frotas-validacao-condutor-print.js?v=20260615a';
+import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615a';
 import './modules/frotas.js';
 
 installDailyDriverResolution(supabase);
 installPrintDriverValidation(supabase);
+installPreviousWeekDefaults(document);
 
 initProtectedPage('Frotas', (content, ctx) => {
   window.FROTAS.openHome(content, {
