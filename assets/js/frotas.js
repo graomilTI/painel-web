a1cf941cd7c296d4f6293cc535ac72e4926aed53
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-15 12:04:43 -0300

    chore: atualizar sincronização automática de frotas

--- assets/js/frotas.js
+++ assets/js/frotas.js
@@ -2,7 +2,7 @@ import { initProtectedPage } from './pageInit.js';
 import { supabase } from './supabaseClient.js';
 import { installDailyDriverResolution } from './frotas-motorista-leitura-diaria.js?v=20260615d';
 import { installPrintDriverValidation } from './frotas-validacao-condutor-print.js?v=20260615a';
-import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615a';
+import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615b';
 import './modules/frotas.js';
 
 installDailyDriverResolution(supabase);
