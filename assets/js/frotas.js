a51abf50ad63d7fd9008837040aac4681c88caf9
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-15 14:02:32 -0300

    fix: carregar dados gravados sem resincronizar

--- assets/js/frotas.js
+++ assets/js/frotas.js
@@ -2,13 +2,13 @@ import { initProtectedPage } from './pageInit.js';
 import { supabase } from './supabaseClient.js';
 import { installDailyDriverResolution } from './frotas-motorista-leitura-diaria.js?v=20260615d';
 import { installPrintDriverValidation } from './frotas-validacao-condutor-print.js?v=20260615a';
-import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615c';
+import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615d';
 import { installIntuitiveFleetLayout } from './frotas-layout-intuitivo.js?v=20260615a';
 import './modules/frotas.js';
 
 installDailyDriverResolution(supabase);
 installPrintDriverValidation(supabase);
-installPreviousWeekDefaults(document);
+installPreviousWeekDefaults(document, supabase);
 installIntuitiveFleetLayout(document);
 
 initProtectedPage('Frotas', (content, ctx) => {
