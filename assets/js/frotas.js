96f4decc46d983b8bdf5297b6b8a3caf1eb148ca
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-15 13:33:21 -0300

    feat: ativar layout intuitivo de excesso de velocidade

--- assets/js/frotas.js
+++ assets/js/frotas.js
@@ -2,12 +2,14 @@ import { initProtectedPage } from './pageInit.js';
 import { supabase } from './supabaseClient.js';
 import { installDailyDriverResolution } from './frotas-motorista-leitura-diaria.js?v=20260615d';
 import { installPrintDriverValidation } from './frotas-validacao-condutor-print.js?v=20260615a';
-import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615b';
+import { installPreviousWeekDefaults } from './frotas-periodo-semana-anterior.js?v=20260615c';
+import { installIntuitiveFleetLayout } from './frotas-layout-intuitivo.js?v=20260615a';
 import './modules/frotas.js';
 
 installDailyDriverResolution(supabase);
 installPrintDriverValidation(supabase);
 installPreviousWeekDefaults(document);
+installIntuitiveFleetLayout(document);
 
 initProtectedPage('Frotas', (content, ctx) => {
   window.FROTAS.openHome(content, {
