f17d350699ae01583a2749ac09ac173167b77475
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-22 12:10:30 -0300

    Usa tela OS mobile dedicada em celulares

--- assets/js/os-page.js
+++ assets/js/os-page.js
@@ -1,4 +1,8 @@
 import { initProtectedPage } from './pageInit.js';
 import { renderOsModule } from './os.js';
+import { renderOsMobile } from './os-mobile.js';
 
-initProtectedPage('OS', (content) => renderOsModule(content));
+initProtectedPage('OS', (content) => {
+  const isMobile = window.matchMedia('(max-width: 900px)').matches;
+  return isMobile ? renderOsMobile(content) : renderOsModule(content);
+});
