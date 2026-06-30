cf166ee0f76447efad70060c404051c50b0c050e
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-30 17:14:17 -0300

    Forca KPIs BFleet com rastreador e ultima posicao

--- assets/js/adm-operacional.js
+++ assets/js/adm-operacional.js
@@ -6,6 +6,7 @@ import './operacional-direcionamento-v4.js';
 import './operacional-direcionamento-v5.js';
 import './operacional-direcionamento-v6.js';
 import './operacional-direcionamento-v7.js';
+import './operacional-direcionamento-v8.js';
 
 initProtectedPage('Operacional ADM', (content, userContext) => {
   if (window.OPERACIONAL?.openHome) {
