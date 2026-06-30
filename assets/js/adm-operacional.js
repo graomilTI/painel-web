cc1cb78235013702cff5b4d520277f8c41424af4
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-06-30 19:25:55 -0300

    Usa v13 com todas as rotas no mapa operacional

--- assets/js/adm-operacional.js
+++ assets/js/adm-operacional.js
@@ -1,6 +1,6 @@
 import { initProtectedPage } from './pageInit.js';
 import './operacional.js';
-import './operacional-direcionamento-v12.js';
+import './operacional-direcionamento-v13.js';
 import './operacional-direcionamento-v12-fix.js';
 import './operacional-direcionamento-v9.js';
 
