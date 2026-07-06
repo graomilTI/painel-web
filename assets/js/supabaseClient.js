5516671c664de78cd856a990cb2fe3a09cffe0bd
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-07-06 10:22:57 -0300

    Update supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const SUPABASE_URL = 'https://jbzmcyycanrlnfhedcup.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impiem1jeXljYW5ybG5maGVkY3VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMzc4MzMsImV4cCI6MjA5ODcxMzgzM30.jr1eEGYx1hg3jJl6oGcHbGY9Kx2vr8YrXUgW_14WA2E';
+export const SUPABASE_ANON_KEY = 'sb_publishable_IyW6rkgdOfEfQh_kx-Z6QQ_cWpdAMte';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
