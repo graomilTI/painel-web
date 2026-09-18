782e89671e549aed79d9dfa499e76bef0f97e4e8
Grão 1000 TI <tecnologia@grao1000.com.br>
2026-09-18 09:57:23 -0300

    chore(programacao): atualiza cache do drawer corrigido

--- assets/js/programacao-gestor-fluxo-avancado.js
+++ assets/js/programacao-gestor-fluxo-avancado.js
@@ -1,5 +1,5 @@
 import { renderProgramacaoSemOs } from './programacao-sem-os.js?v=20260911-finaliza-libera-semos1';
-import { renderProgramacaoListaDrawer } from './programacao-lista-drawer.js?v=20260917-deslocamento-persistido1-idconfirmado1';
+import { renderProgramacaoListaDrawer } from './programacao-lista-drawer.js?v=20260918-os-reaproveitada-dia-atual1';
 import { TODAS_SUPERVISOES } from './programacao-gestor-filtro-fix.js';
 
 // Programação Gestor (2026-07-21, "lista + painel lateral"): o botão Carregar
